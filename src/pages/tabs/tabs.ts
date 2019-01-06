65eb82b405b34946c41f2ca99bcde430802fe620
Józef Bongowski <j.bongowski@protonmail.com>
2019-01-06 22:35:28 +0100

    Add loading popup

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { ProfilePage } from '../profile/profile';
 import { AnnoucementsPage } from "../annoucements/annoucements";
 import { ClassesPage } from "../classes/classes";
+import { LoadingController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -13,7 +14,17 @@ export class TabsPage {
   tab2Root = AnnoucementsPage;
   tab3Root = ClassesPage;
 
-  constructor() {
+  constructor(public loadingCtrl: LoadingController) { }
 
+  ionViewDidLoad() {
+    this.loginLoading();
+  }
+
+  loginLoading() {
+    const loader = this.loadingCtrl.create({
+      content: "Trwa logowanie...",
+      duration: 2000
+    });
+  loader.present();
   }
 }
